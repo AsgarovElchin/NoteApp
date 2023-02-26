c136ad669010235be7356a706987974f73f9d4b5
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2023-02-26 16:51:22 +0400

    feature-NA-noteDao interface added

---
+++ app/src/main/java/com/example/noteapp/data/NoteDao.kt
@@ -0,0 +1,16 @@
+package com.example.noteapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<NoteData>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertData(noteData: NoteData)
+}
