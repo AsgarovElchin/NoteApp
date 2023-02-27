8341b2535237bdae151423a2d2a82f23635cb1b5
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2023-02-27 14:40:13 +0400

    feature-NA-NoteViewModel added

---
+++ app/src/main/java/com/example/noteapp/data/viewmodel/NoteViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.noteapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.noteapp.data.NoteDatabase
+import com.example.noteapp.data.models.NoteData
+import com.example.noteapp.data.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private val noteDao = NoteDatabase.getDatabase(
+        application
+    ).noteDao()
+
+    private val repository: NoteRepository
+
+    private val getAllData: LiveData<List<NoteData>>
+
+    init {
+        repository = NoteRepository((noteDao))
+        getAllData = repository.getAllData()
+    }
+
+    fun insertData(noteData: NoteData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(noteData)
+        }
+    }
+}
